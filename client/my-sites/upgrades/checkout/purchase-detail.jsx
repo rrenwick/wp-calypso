fa2be64b5bc4f2cfd74e880278ee42a9a3ea35b2
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-01-26 17:11:12 -0800

    Checkout: Don't open internal links in a new window
    Fixes #2810.

--- client/my-sites/upgrades/checkout/purchase-detail.jsx
+++ client/my-sites/upgrades/checkout/purchase-detail.jsx
@@ -8,13 +8,13 @@ import React from 'react';
  */
 import Button from 'components/button';
 
-const PurchaseDetail = ( { additionalClass, buttonText, description, onButtonClick, title } ) => (
+const PurchaseDetail = ( { additionalClass, buttonText, description, href, target, title } ) => (
 	<li className={ 'checkout__purchase-detail ' + additionalClass }>
 		<div className="checkout__purchase-detail-text">
 			<h3 className="checkout__purchase-detail-title">{ title }</h3>
 			<p className="checkout__purchase-detail-description">{ description }</p>
 		</div>
-		<Button onClick={ onButtonClick } primary>
+		<Button href={ href } target={ target } primary>
 			{ buttonText }
 		</Button>
 	</li>
@@ -24,7 +24,8 @@ PurchaseDetail.propTypes = {
 	additionalClass: React.PropTypes.string,
 	buttonText: React.PropTypes.string.isRequired,
 	description: React.PropTypes.string.isRequired,
-	onButtonClick: React.PropTypes.func.isRequired,
+	href: React.PropTypes.string,
+	target: React.PropTypes.string,
 	title: React.PropTypes.string.isRequired
 };
 
