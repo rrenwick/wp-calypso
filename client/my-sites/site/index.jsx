ba66b4eae18df0f4a19a4e40df034c1e8b09c4b5
Matías Ventura <mv@matiasventura.com>
2016-02-03 17:00:18 +0100

    Clicking in star-badge should trigger main site action.

--- client/my-sites/site/index.jsx
+++ client/my-sites/site/index.jsx
@@ -181,6 +181,11 @@ module.exports = React.createClass( {
 								<Gridicon icon="house" size={ 18 } />
 							</span>
 						}
+						{ ! this.props.enableActions && sites.isStarred( this.props.site ) &&
+							<span className="site__star-badge">
+								<Gridicon icon="star" size={ 18 } />
+							</span>
+						}
 					</a>
 				:
 					<div className="site__content">
@@ -199,11 +204,6 @@ module.exports = React.createClass( {
 					>
 						<Gridicon icon="ellipsis" size={ 24 } />
 					</button>
-				}
-				{ ! this.props.enableActions && sites.isStarred( this.props.site ) &&
-					<span className="site__star-badge">
-						<Gridicon icon="star" size={ 18 } />
-					</span>
 				}
 			</div>
 		);
